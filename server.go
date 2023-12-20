eab5ff7e9ef1588ce25f01ceba570d931b27ca61
vlruiz108 <vlruiz@hotmail.com>
2023-12-19 21:04:40 -0300

    chore: adicao de parametros de alerta

--- server.go
+++ server.go
@@ -1,5 +1,7 @@
+// Pacote principal da aplicação em Go
 package main
 
+// Importação das bibliotecas necessárias
 import (
    "fmt"
    "net/http"
@@ -8,6 +10,7 @@ import (
    "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+// Definição de um contador Prometheus para contabilizar as requisições ao manipulador "/ping"
 var pingCounter = prometheus.NewCounter(
    prometheus.CounterOpts{
        Name: "ping_request_count",
@@ -15,15 +18,24 @@ var pingCounter = prometheus.NewCounter(
    },
 )
 
+// Função que responde a requisições HTTP com "pong" e incrementa o contador
 func ping(w http.ResponseWriter, req *http.Request) {
    pingCounter.Inc()
    fmt.Fprintf(w, "pong")
 }
 
+// Função principal que registra o contador Prometheus, configura manipuladores para as rotas "/ping" e "/metrics",
+// e inicia o servidor HTTP na porta 8090
 func main() {
+   // Registro do contador Prometheus
    prometheus.MustRegister(pingCounter)
 
+   // Configuração do manipulador para a rota "/ping"
    http.HandleFunc("/ping", ping)
+
+   // Configuração do manipulador para a rota "/metrics" para expor métricas Prometheus
    http.Handle("/metrics", promhttp.Handler())
+
+   // Inicia o servidor HTTP na porta 8090
    http.ListenAndServe(":8090", nil)
 }
